bc6d19e06bc3c0f08c9cde0a2d51cfeaf0e470f5
T V V Sai Karthik <114074156+tanuku-saikarthik@users.noreply.github.com>
2025-04-08 00:34:51 +0530

    Update supabaseClient.js

--- server/supabaseClient.js
+++ server/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient }from '@supabase/supabase-js';
 
 // Read environment variables
 const supabaseUrl = process.env.SUPABASE_URL||'https://yivzvfseqfazsesqyjks.supabase.co';
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY||'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlpdnp2ZnNlcWZhenNlc3F5amtzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDU4MzcyNSwiZXhwIjoyMDU2MTU5NzI1fQ.XGeVwUlakiK0yaGe3nRSedKBOdr5AGc2qvYyAM-4uPI';
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 // Check if keys exist
 if (!supabaseUrl || !supabaseAnonKey) {
